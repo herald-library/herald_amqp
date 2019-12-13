cb7ae0974e94a6cc4ffc1fc2b108d0d84de2d8b4
Thiago Henrique <contato@thiagohenrique.com.br>
2019-12-13 17:19:34 -0300

    This app dont have supervision

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Herald.AMQP.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Herald.AMQP.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
